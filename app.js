569ccc68e5a0c7adc4bbd6cef652c6635e02f048
Duncan Sterken <duncan.sterken123@gmail.com>
2020-02-04 19:50:29 +0100

    It has a cool tempalte now

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use('assets/', express.static('assets'));
+app.use('temp/', express.static('temp'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.sendFile(__dirname + '/pages/home.html');
 });
 
 app.listen(process.env.PORT || 3000);
