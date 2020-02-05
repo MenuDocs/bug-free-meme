3fa1772cbc564ff4a930711b6612e366c116db53
Duncan Sterken <duncan.sterken123@gmail.com>
2020-02-05 13:24:28 +0100

    Change front page

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.use('assets/', express.static('assets'));
-app.use('temp/', express.static('temp'));
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/pages/home.html');
 });
